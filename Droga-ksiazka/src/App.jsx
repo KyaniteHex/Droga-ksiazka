e1fb81ff123c4c613f656addfb65a1cb7ddb6467
KyaniteHex <lorenspawel@gmail.com>
2024-05-07 12:28:14 +0200

    update

--- Droga-ksiazka/src/App.jsx
+++ Droga-ksiazka/src/App.jsx
@@ -8,20 +8,16 @@ function App() {
 
 	return (
 		<>
-			<div className='container pt-2 text-center bg-white w-4/6 h-screen'>
+			<div className='container pt-2 bg-white w-4/6 grid place-items-center'>
 				<header>
-					<div className='header-top flex h-56 mb-6 items-center'>
-						<div className='logo '>
-							<a href='#' className='pt-2 block'>
-								<img
-									src='src\img\logo-no-background.png'
-									alt='Logo'
-									className='w-48'
-								/>
+					<div className='header-top h-56 mb-6 grid grid-cols-4 place-items-center w-full'>
+						<div className='logo w-52'>
+							<a href='#'>
+								<img src='src\img\logo-no-background.png' alt='Logo' />
 							</a>
 						</div>
-						<div className='search-bar w-4/6'>
-							<span className='ml-4 pl-2.5 border-l border-t border-b border-gray-300 rounded-md text-start items-end h-12 text-3xl'>
+						<div className='search-bar w-full col-span-3'>
+							<span className='ml-4 pl-2.5 border-l border-t border-b border-gray-300 rounded-md text-start h-12 text-3xl '>
 								<input
 									type='text'
 									placeholder='Wyszukaj książki w naszej bazie'
@@ -33,8 +29,8 @@ function App() {
 							</span>
 						</div>
 					</div>
-					<nav>
-						<ul className='bg-[#d28140] rounded-xl w-11/12 mx-auto flex justify-center text-[#f1eee8]'>
+					<nav className='grid place-items-center'>
+						<ul className='bg-[#d28140] rounded-xl w-11/12 text-[#f1eee8] gap-6 grid grid-cols-6'>
 							<li>
 								<a
 									href='http://'
@@ -98,15 +94,15 @@ function App() {
 						</ul>
 					</nav>
 				</header>
-				<section className='main mt-20 w-full '>
-					<div className='book-container border-t border-r border-l p-3 border-gray-300 rounded-lg grid grid-cols-[30%_40%]'>
+				<section className='main mt-20'>
+					<div className='book-container border-t border-r border-l p-3 border-gray-300 rounded-lg grid grid-cols-4'>
 						<div className='book-img'>
 							<img
-								src='src/img/Pan Lodowego Ogrodu.jpg'
+								src='src/img/pan-lodowego-ogrodu-1.jpg'
 								alt='Pan Lodowego ogrodu'
 							/>
 						</div>
-						<div className='book-info pt-5'>
+						<div className='book-info pt-5 col-span-2'>
 							<span className='book-name text-2xl font-semibold'>
 								Pan Lodowego Ogrodu
 							</span>
@@ -125,7 +121,7 @@ function App() {
 								<span className='mr-5'>Średnia: 4.9</span>
 								<span>Ilość ocen: 34</span>
 							</p>
-							<div className='book-info-other grid grid-cols-[150px_minmax(900px,1fr)] grid-flow-row mt-5 text-start'>
+							<div className='book-info-other grid grid-cols-2'>
 								<span>Wydawnictwo: Fabryka słów</span>
 								<span>Format: 15.0x22.cm</span>
 								<span>Seria: Pan lodowego ogrodu</span>
@@ -139,10 +135,10 @@ function App() {
 										<i class='fa-solid fa-arrow-down-long mt-2'></i>
 									</span>
 								</div>
-								<span>
+								<button>
 									Pan Lodowego Ogrodu.epub
 									<i class='fa-solid fa-download text-4xl mt-5'></i>
-								</span>
+								</button>
 							</div>
 						</div>
 					</div>
@@ -180,12 +176,15 @@ function App() {
 							</span>
 						</div>
 					</section>
-					<section className='others-books border border-t-gray-300 mt-5'>
-						<div className='other-books-header h-20 mt-5'>
-							<span className='other-books-title text-2xl inline-block w-3/4 text-start'>
-								Inne książki:{' '}
-							</span>
-							<span className='other-books-navigation-btn inline-block w-1/4 text-end'>
+				</section>
+				<section className='others-books-container mt-10 w-10/12'>
+					<div className='book-desctibe-line border-t border-gray-300 h-0'></div>
+					<div className='other-books-header mb-5 mt-10 pb-3 grid grid-cols-2 border-b border-gray-300'>
+						<div className='other-books-title text-start'>
+							<span className='text-2xl'>Inne książki:</span>
+						</div>
+						<div className='other-books-nav-btn text-end'>
+							<span className='other-books-navigation-btn'>
 								<div className='slider-book'></div>
 								<button className='border-gray-300 border w-10 h-10 p-1 text-2xl rounded-md'>
 									<i class='fa-solid fa-angle-left'></i>
@@ -195,14 +194,76 @@ function App() {
 								</button>
 							</span>
 						</div>
-            <div className="other-book-title-slider">
-              
-            </div>
-					</section>
-					<section className='reviews'>
-						<div className='reviews-comment'></div>
-						<div className='reviews-add-comment'></div>
-					</section>
+					</div>
+					<div className='other-books-body grid place-items-center'>
+						<div className='other-book-title-slider w-10/12 grid grid-cols-4  gap-10'>
+							<div className='other-book-slider-element'>
+								<img
+									src='src\img\wojna-makowa-1.jpg'
+									className='slider-book-img'
+									alt=''
+								/>
+								<span className='slider-book-name'>Wojna Makowa tom 1</span>
+								<span className='rates'>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star-half-stroke text-orange-600'></i>
+								</span>
+							</div>
+							<div className='other-book-slider-element'>
+								<img
+									src='src\img\faceci-w-gumofilcach.jpg'
+									className='slider-book-img'
+									alt=''
+								/>
+								<span className='slider-book-name'>Faceci w gumofilcach</span>
+								<span className='rates'>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star-half-stroke text-orange-600'></i>
+								</span>
+							</div>
+							<div className='other-book-slider-element'>
+								<img
+									src='src\img\percy-jackson-zlodziej-pioruna.jpg'
+									className='slider-book-img'
+								/>
+								<span className='slider-book-name'>
+									Percy Jackson i bogowie olimpijscy: Złodziej pioruna
+								</span>
+								<span className='rates'>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star-half-stroke text-orange-600'></i>
+								</span>
+							</div>
+							<div className='other-book-slider-element'>
+								<img
+									src='src\img\th-big-short.jpg'
+									alt='The'
+									className='slider-book-img'
+								/>
+								<span className='slider-book-name'>The Big Short</span>
+								<span className='rates'>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star text-orange-600'></i>
+									<i className='fa-solid fa-star-half-stroke text-orange-600'></i>
+								</span>
+							</div>
+						</div>
+					</div>
+				</section>
+				<section className='reviews'>
+					<div className='reviews-comment'></div>
+					<div className='reviews-add-comment'></div>
 				</section>
 			</div>
 		</>
