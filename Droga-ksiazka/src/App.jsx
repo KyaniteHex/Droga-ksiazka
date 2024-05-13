77e83ad2d905edfa569ff45af1dcd6ccaaa5d822
Paweł <103453334+KyaniteHex@users.noreply.github.com>
2024-05-13 12:25:22 +0200

    Rates part1

--- Droga-ksiazka/src/App.jsx
+++ Droga-ksiazka/src/App.jsx
@@ -1,7 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -36,8 +36,7 @@ function App() {
 									href='http://'
 									target='_blank'
 									rel='noopener noreferrer'
-									className='inline-block px-4 py-2'
-								>
+									className='inline-block px-4 py-2'>
 									Bestsellery
 								</a>
 							</li>
@@ -46,8 +45,7 @@ function App() {
 									href='http://'
 									target='_blank'
 									rel='noopener noreferrer'
-									className='inline-block px-4 py-2'
-								>
+									className='inline-block px-4 py-2'>
 									Premiery i nowości
 								</a>
 							</li>
@@ -56,8 +54,7 @@ function App() {
 									href='http://'
 									target='_blank'
 									rel='noopener noreferrer'
-									className='inline-block px-4 py-2'
-								>
+									className='inline-block px-4 py-2'>
 									Uwielbiane przez redakcję
 								</a>
 							</li>
@@ -66,8 +63,7 @@ function App() {
 									href='http://'
 									target='_blank'
 									rel='noopener noreferrer'
-									className='inline-block px-4 py-2'
-								>
+									className='inline-block px-4 py-2'>
 									Fantastyka
 								</a>
 							</li>
@@ -76,8 +72,7 @@ function App() {
 									href='http://'
 									target='_blank'
 									rel='noopener noreferrer'
-									className='inline-block px-4 py-2'
-								>
+									className='inline-block px-4 py-2'>
 									Dla dzieci
 								</a>
 							</li>
@@ -86,8 +81,7 @@ function App() {
 									href='http://'
 									target='_blank'
 									rel='noopener noreferrer'
-									className='inline-block px-4 py-2'
-								>
+									className='inline-block px-4 py-2'>
 									Dodaj książkę
 								</a>
 							</li>
@@ -112,7 +106,6 @@ function App() {
 							<p className='book-rate mt-4 text-s'>
 								<span className='mr-5'>
 									Ocena:
-									<i className='fa-solid fa-star text-orange-600'></i>
 									<i className='fa-solid fa-star text-orange-600'></i>
 									<i className='fa-solid fa-star text-orange-600'></i>
 									<i className='fa-solid fa-star text-orange-600'></i>
@@ -261,9 +254,80 @@ function App() {
 						</div>
 					</div>
 				</section>
-				<section className='reviews'>
-					<div className='reviews-comment'></div>
-					<div className='reviews-add-comment'></div>
+				<section className='reviews w-10/12 mt-10 border-t border-gray-300'>
+					<div className='reviewes-title mt-10 text-start'>
+						<span className='text-2xl'>Recenzje: </span>
+					</div>
+					<div className='reviews-summary flex place-items-center mt-5'>
+						<div className=''>
+							<div className='rate-rounded rounded-full border border-orange-400 w-32 h-32 flex place-items-center place-content-center'>
+								<div
+									className='bg-orange-200 rounded-full place-content-center'
+									style={{ height: "90%", width: "90%" }}>
+									<span className='rate-ratio text-xl font-bold'>4.9</span>
+								</div>
+							</div>
+						</div>
+						<div className='w-full'>
+							<div className='rate-stars-scale grid grid-rows-5 gap-3 mt-5 place-items-end'>
+								<div class='rating-slider flex w-3/4'>
+									<span class='text-sm'>
+										5 <i className='fa-solid fa-star text-orange-400'></i>
+									</span>
+									<div class='bg-orange-200 mx-2 w-2/3 rounded-lg'>
+										<div class='bg-orange-400 w-6/12 h-full rounded-lg'></div>
+									</div>
+									<span class='flex'>50%</span>
+								</div>
+
+								<div className='rating-slider flex w-3/4'>
+									<span className='text-sm'>
+										4 <i className='fa-solid fa-star text-orange-400'></i>
+									</span>
+									<div className='bg-orange-200 mx-2 w-2/3 rounded-lg'>
+										<div className='bg-orange-400 w-4/12 h-full rounded-lg'></div>
+									</div>
+									<span className='flex'>25%</span>
+								</div>
+								<div className='rating-slider flex w-3/4'>
+									<span className='text-sm'>
+										3 <i className='fa-solid fa-star text-orange-400'></i>
+									</span>
+									<div className='bg-orange-200 mx-2 w-2/3 rounded-lg'>
+										<div className='bg-orange-400  w-2/12 h-full rounded-lg'></div>
+									</div>
+									<span className='flex'>15%</span>
+								</div>
+								<div className='rating-slider flex w-3/4'>
+									<span className='text-sm'>
+										2 <i className='fa-solid fa-star text-orange-400'></i>
+									</span>
+									<div className='bg-orange-200 mx-2 w-2/3 rounded-lg'>
+										<div className='bg-orange-400  w-1/12 h-full rounded-lg'></div>
+									</div>
+									<span className='flex'>5%</span>
+								</div>
+								<div className='rating-slider flex w-3/4'>
+									<span className='text-sm'>
+										1 <i className='fa-solid fa-star text-orange-400'></i>
+									</span>
+									<div className='bg-orange-200 mx-2 w-2/3 rounded-lg'>
+										<div className='bg-orange-400 w-0 h-full rounded-lg'></div>
+									</div>
+									<span className='flex'>0</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className='add-recent w-3/4 mt-14 h-52'>
+						<div className='reviews-comment mt-10 grid grid-cols-2'>
+							<span>Czytałeś? Możesz podzielić się swoją opinią</span>
+							<button className='bg-red-400 rounded-md w-16 text-white-400'>
+								Dodaj recenzję
+							</button>
+						</div>
+						<div className='reviews-add-comment'></div>
+					</div>
 				</section>
 			</div>
 		</>
